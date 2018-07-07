edb8a51785d09d579029ed98efbbdf69271e7d5a
bigman212 <bigman212@yandex.ru>
2018-07-07 21:39:54 +1000

    fix bug with router and link

--- create_competition-master/src/App.js
+++ create_competition-master/src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
-
-//import InputInfo from './components/InputInfo.js'
-//import TabelComp from './components/TabelComp.js'
+import React, {Component} from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Competitions from './pages/Competitions.jsx';
 import Promptly from './pages/Promptly.jsx';
 import Regulation from './pages/Regulation.jsx';
 import Users from './pages/Users.jsx';
+
 class App extends Component {
   render() {
     return (
-
-            <Router>
-               <div>
-                 <Route exact path="/" component={Competitions}/>
-                 <Route path="/promptly" component={Promptly}/>
-                 <Route path="/regulation" component={Regulation}/>
-                 <Route path="/users" component={Users}/>
-                </div>
-            </Router>
-   
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Competitions}/>
+            <Route path="/promptly" component={Promptly}/>
+            <Route path="/regulation" component={Regulation}/>
+            <Route path="/users" component={Users}/>
+          </Switch>
+        </Router>
     );
   }
 }
--- create_competition-master/src/components/Navbar.jsx
+++ create_competition-master/src/components/Navbar.jsx
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 class Navbar extends Component {
-    render() {
-      return (
+  render() {
+    return (
         <nav className="navbar navbar-toggleable-md">
-          <button className="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse"
+                  data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                  aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="container">
             <Link className="navbar-brand" to="/">Enduro</Link>
             <ul className="nav flex-column">
-                 <li className="nav-item active">
-                   <Link className="nav-link" to="/">Соревнования <span className="sr-only">(current)</span></Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link className="nav-link" to="/promptly">Срочное объявление</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link className="nav-link" to="/regulation">Регламент</Link>
-                 </li>
-                 <li className="nav-item">
-                  <Link className="nav-link" to="/users">Пользователи</Link>
-                 </li>
-              </ul>
-            
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Соревнования <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/promptly">Срочное объявление</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/regulation">Регламент</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/users">Пользователи</Link>
+              </li>
+            </ul>
           </div>
         </nav>
-      );
-    }
+    );
   }
-  
-  export default Navbar;
+}
+
+export default Navbar;
